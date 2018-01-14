2e71c5e255c478c618d84adb00b5e31163688bc8
wowbob396 <wowbob396@gmail.com>
2018-01-14 15:42:31 -0600

    created testview to create view for the charts

--- components/TestView.js
+++ components/TestView.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    chart: {
+        width: 200,
+        height: 200,
+    },
+});
+
+const data = [[
+    [0,1],
+    [1,3],
+    [3,7],
+    [4,9],
+]];
+
 class TestView extends React.Component {
     constructor(props) {
         super(props);
@@ -9,7 +29,15 @@ class TestView extends React.Component {
         }
     }
 
-    componentDidMount() {
-        return fetch('https://min-api.cryptocompare.com/data/price?fsym=ADA&tsyms=USD')
+ //   componentDidMount() {
+//        return fetch('https://min-api.cryptocompare.com/data/price?fsym=ADA&tsyms=USD')
+//    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+ 
+            </View>
+        );
     }
 }
