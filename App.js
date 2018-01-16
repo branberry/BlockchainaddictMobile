50b6cfa898d13758010db10172770f386b41fb5e
Brandon <wowbob396@gmail.com>
2018-01-15 20:39:26 -0600

    created logo

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import TestView from './components/TestView.js';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.titleText}>Hi moo!</Text>
+      <Text style={styles.titleText}>Hello {this.props.name}!</Text>
+    );
+  }
+ }
+
+ class Blink extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {isShowingText: true};
+   }
+
+   render() {
+     return (
+       <Text>Hi</Text>
+     );
+   }
+ }
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={styles.container}>
+          <Text style={styles.titleText}>Blockchain Addict </Text>
+        </View>
+        <View style={styles.subContainer}>
+        </View>
       </View>
     );
   }
@@ -15,12 +39,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2E4057',
+   
     alignItems: 'center',
     justifyContent: 'center',
   },
-
+  subContainer: {
+    flex: 8,
+    backgroundColor: '#FFFFFF',
+  },
   titleText : {
-    fontSize: 60,
+    fontSize: 30,
+    color:'#CCCCCC', 
   },
 });
